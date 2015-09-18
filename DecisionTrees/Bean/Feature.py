34803f4d373312dafab03fa8fb8c746f1c79af49
niharkhetan <nihar.khetan@gmail.com>
2015-09-18 16:46:34 -0400

    Enhance the feature object
    Change-Id: I3ebdedcf0898213b72147a1517306129b3819f24
    Signed-off-by: niharkhetan <nihar.khetan@gmail.com>

--- DecisionTrees/Bean/Feature.py
+++ DecisionTrees/Bean/Feature.py
@@ -17,6 +17,7 @@ class Feature(object):
         self.__data__ = data
         self.__count__ = len(data)
         self.__isClassLabel__ = isClassLabel
+        self.__discreteSet__ = self.__calculateDiscreteSet__(data)
     
     def getName(self):
         return self.__name__
@@ -28,4 +29,22 @@ class Feature(object):
         return self.__count__
     
     def isClassLabel(self):
-        return self.__isClassLabel__    
+        return self.__isClassLabel__
+    
+    def getDiscreteSet(self):
+        return self.__discreteSet__
+    
+    def __calculateDiscreteSet__(self, data):
+        discreteSet = []
+        for eachType in data:
+            if eachType not in discreteSet:
+                discreteSet.append(eachType)
+        return discreteSet
+    
+if __name__ == '__main__':
+    f1 = Feature("Feature1", [1,1,1,1,1,1,1,1,0,0,3,4,4,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0])
+    print f1.getName()
+    print f1.getData()
+    print f1.getCount()
+    print f1.isClassLabel()
+    print f1.getDiscreteSet()
