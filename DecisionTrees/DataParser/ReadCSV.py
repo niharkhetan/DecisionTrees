381b9de82a688fadc246e3c92e2e7af8e5fd3c36
Ghanshyam Malu <malughanshyam@gmail.com>
2015-09-18 18:33:41 -0400

    Moved dataset into a new package Modified ReadCSV file to read files from different package

--- DecisionTrees/DataParser/ReadCSV.py
+++ DecisionTrees/DataParser/ReadCSV.py
@@ -5,26 +5,30 @@ Created on Sep 18 , 2015
 '''
 
 import csv, sys
+import os, Dataset
+
 
 def readFileAsVector(csvFileName):
     ''' 
     Reads a data in CSV file and returns a vector dataset
     '''
     vector = []
-    
-    with open(csvFileName, 'rb') as csvFile:
-        reader = csv.reader(csvFile)
-        try:
-            for row in reader:
-                vector.append(row)
-        except csv.Error as e:
-            sys.exit('file %s, line %d: %s' % (csvFile, reader.line_num, e))
-        except IOError as e:
-            sys.exit('Could not read file %s, line %d: %s' % (csvFile, reader.line_num, e))
+    csvFileName = os.path.join(Dataset.__path__[0], csvFileName)
+    try:
+        with open(csvFileName, 'rb') as csvFile:
+            reader = csv.reader(csvFile)
+            try:
+                for row in reader:
+                    vector.append(row)
+            except csv.Error as e:
+                sys.exit('file %s, line %d: %s' % (csvFileName, reader.line_num, e))
+    except IOError as e:
+        sys.exit(e)
             
     return vector
 
-if __name__ == '__main__':
+
+def main():
     training_data = "zoo-train.csv"
     vector = readFileAsVector(training_data)
     #print vector  
@@ -38,3 +42,6 @@ if __name__ == '__main__':
     for row in vector[1:]:
         print  "\t".join(row) 
         
+    
+if __name__ == '__main__':
+    main()
