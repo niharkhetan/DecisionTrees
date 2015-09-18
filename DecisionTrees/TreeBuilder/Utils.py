c67b79c2f3ee5b0e39d7b38f8e97d31f04fd23d7
niharkhetan <nihar.khetan@gmail.com>
2015-09-18 19:55:41 -0400

    converting vector to column
    Change-Id: Id0c824c39f894fe54ec96b2055694489f1591975
    Signed-off-by: niharkhetan <nihar.khetan@gmail.com>

--- DecisionTrees/TreeBuilder/Utils.py
+++ DecisionTrees/TreeBuilder/Utils.py
@@ -5,12 +5,20 @@ Created on Sep 17, 2015
 '''
 
 import numpy as np
+from DataParser.ReadCSV import *
+from Bean.Feature import Feature
 
-def getEntropy(positive, negative):
-    pos = positive / float(positive + negative)
-    neg = negative / float(positive + negative)
-    entropy = -1*(pos*np.log2(pos)) - (neg*np.log2(neg))
-    return entropy
+
+def getEntropy(feature):
+    n = feature.getCount()
+    set = feature.getDiscreteSet()
+    keys = set.keys()
+    values = set.values()
+    entropy = 0
+
+    for v in values:
+        entropy += (v / float(n)) * (np.log2(v / float(n)))
+    return entropy*(-1) 
 
 def getInformationGain(entropy, entropySv):
     '''entropySv is a list of tuple (x, y, z):
@@ -23,7 +31,19 @@ def getInformationGain(entropy, entropySv):
     informationGain = entropy + sumSv
     return informationGain
 
+def convertVectorToColumnar(vector):
+    '''This returns the whole data as a list of features objects'''
+    columns = []
+    columnLabels = vector[0]
+    for column in range(0, len(columnLabels)):
+        columnData = []
+        for row in vector[1:]:
+            columnData.append(row[column])
+        columns.append(Feature(columnLabels[column], columnData))
+    return columns
+
 if __name__ == '__main__':
+    '''
     positive = 5
     negative = 9
     entropy = getEntropy(positive, negative)
@@ -31,3 +51,9 @@ if __name__ == '__main__':
     
     print getInformationGain(entropy, [(14, 3, 4),(14, 6, 1)])
     print getInformationGain(entropy, [(14, 6, 2),(14, 3, 3)])
+    '''
+    training_data = "../DataParser/zoo-train.csv"
+    vector = readFileAsVector(training_data)
+    columns = convertVectorToColumnar(vector)
+    classLabel = columns[16]
+    print getEntropy(columns[16])
